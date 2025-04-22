bf615946eb8a44959cfa1f5b5be5a8e319bf09a8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-22 19:48:26 -0300

    ✨ feat: cria rota GET para listar todos os produtos

---
+++ src/app/api/produtos/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server'
+import { getTodosProdutos } from '@/lib/api'
+
+export const dynamic = 'force-static'
+
+export async function GET() {
+    const produtos = getTodosProdutos()
+
+    return NextResponse.json({ produtos })
+}
