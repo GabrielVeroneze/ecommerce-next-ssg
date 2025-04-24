55b5701dbd3d0754177299ee5c20335f07ba95eb
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-24 18:54:51 -0300

    🔧 chore: habilita trailingSlash para compatibilidade com exportação estática

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     output: 'export',
-    assetPrefix: '/out',
+    trailingSlash: true,
     images: {
         remotePatterns: [
             {
